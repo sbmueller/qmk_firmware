e3bbb64c455f41e568059fbdadeec8837ba473b2
Sebastian Müller <gsenpo@gmail.com>
2021-05-28 13:23:56 +0200

    Add Rotary Encoder

--- keyboards/preonic/keymaps/sbmueller/keymap.c
+++ keyboards/preonic/keymaps/sbmueller/keymap.c
@@ -50,15 +50,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Alt  | GUI  | RGBT |Lower |    Space    |Raise |MMute | Mute | Vol- | Vol+ |
+ * | Mute | Ctrl | Alt  | CMD  |Lower |    Space    |Raise |MMute | Shrug|Dpoint| RGBT |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid(
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,       KC_5,   KC_6,   KC_7,       KC_8,      KC_9,    KC_0,    KC_BSPC,
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,       KC_T,   KC_Y,   KC_U,       KC_I,      KC_O,    KC_P,    KC_ENT,
-  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,       KC_G,   KC_H,   KC_J,       KC_K,      KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,       KC_B,   KC_N,   KC_M,       KC_COMM,   KC_DOT,  KC_SLSH, KC_RSFT,
-  KC_LCTL, KC_LALT, KC_LGUI, RGB_TOG, MO(_LOWER), KC_SPC, KC_SPC, MO(_RAISE), RCS(KC_M), KC_MUTE, KC_VOLD, KC_VOLU
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,       KC_5,   KC_6,   KC_7,       KC_8,      KC_9,   KC_0,    KC_BSPC,
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,       KC_T,   KC_Y,   KC_U,       KC_I,      KC_O,   KC_P,    KC_ENT,
+  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,       KC_G,   KC_H,   KC_J,       KC_K,      KC_L,   KC_SCLN, KC_QUOT,
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,       KC_B,   KC_N,   KC_M,       KC_COMM,   KC_DOT, KC_SLSH, KC_RSFT,
+  KC_MUTE, KC_LCTL, KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC, KC_SPC, MO(_RAISE), RCS(KC_M), SHRUG,  DPOINT,  RGB_TOG
 ),
 
 /* Colemak - deprecated
@@ -111,7 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Del  | RGB+ |RGBH+ |RGBV+ |      |      | LEFT | DOWN |  UP  |RIGHT |   Ö  |  Ä   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      | RGB- |RGBH- |RGBV- |      |      |      |PgDown| PgUp | Home | End  |      |
+ * |      | RGB- |RGBH- |RGBV- |UC_MOD|      |      |PgDown| PgUp | Home | End  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      | RGBT |      |             |ADJUST| Mute | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
@@ -120,7 +120,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TILD, KC_EXLM,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,   KC_ASTR, KC_LPRN, ALGR(KC_S), KC_DEL,
   KC_TILD, KC_EXLM,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,   KC_ASTR, KC_LPRN, ALGR(KC_Y), KC_NO,
   KC_DEL,  RGB_RMOD, RGB_HUI, RGB_VAI, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN,   KC_UP,   KC_RGHT, ALGR(KC_P), ALGR(KC_Q),
-  KC_TRNS, RGB_MOD,  RGB_HUD, RGB_VAD, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGDOWN, KC_PGUP, KC_HOME, KC_END,     KC_TRNS,
+  KC_TRNS, RGB_MOD,  RGB_HUD, RGB_VAD, UC_MOD,  KC_TRNS, KC_TRNS, KC_PGDOWN, KC_PGUP, KC_HOME, KC_END,     KC_TRNS,
   KC_TRNS, KC_TRNS,  KC_TRNS, RGB_TOG, KC_TRNS, KC_TRNS, KC_TRNS, MO(5),     KC_MUTE, KC_VOLD, KC_VOLU,    KC_MPLY
 ),
 
@@ -271,11 +271,9 @@ void encoder_update_user(uint8_t index, bool clockwise) {
     }
   } else {
     if (clockwise) {
-      register_code(KC_PGDN);
-      unregister_code(KC_PGDN);
+      tap_code(KC_VOLU);
     } else {
-      register_code(KC_PGUP);
-      unregister_code(KC_PGUP);
+      tap_code(KC_VOLD);
     }
   }
 }
