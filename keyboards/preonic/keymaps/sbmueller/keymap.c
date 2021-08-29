1e87c420168a0442e10c63b0976d5729770c52a3
Sebastian Müller <gsenpo@gmail.com>
2021-08-29 12:25:20 +0200

    Update qmk

--- keyboards/preonic/keymaps/sbmueller/keymap.c
+++ keyboards/preonic/keymaps/sbmueller/keymap.c
@@ -254,7 +254,7 @@ uint16_t muse_counter = 0;
 uint8_t muse_offset = 70;
 uint16_t muse_tempo = 50;
 
-void encoder_update_user(uint8_t index, bool clockwise) {
+bool encoder_update_user(uint8_t index, bool clockwise) {
   if (muse_mode) {
     if (IS_LAYER_ON(_RAISE)) {
       if (clockwise) {
@@ -276,9 +276,10 @@ void encoder_update_user(uint8_t index, bool clockwise) {
       tap_code(KC_VOLD);
     }
   }
+  return true;
 }
 
-void dip_switch_update_user(uint8_t index, bool active) {
+bool dip_switch_update_user(uint8_t index, bool active) {
     switch (index) {
         case 0:
             if (active) {
@@ -294,6 +295,7 @@ void dip_switch_update_user(uint8_t index, bool active) {
                 muse_mode = false;
             }
     }
+    return true;
 }
 
 
