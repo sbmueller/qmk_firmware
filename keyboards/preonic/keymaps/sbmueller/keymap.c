6594de3f814610f66a822f0962635cb84b399118
Sebastian Müller <gsenpo@gmail.com>
2021-10-25 16:15:12 +0200

    Update

--- keyboards/preonic/keymaps/sbmueller/keymap.c
+++ keyboards/preonic/keymaps/sbmueller/keymap.c
@@ -149,7 +149,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Reset| Debug|      |      |      |      |TermOf|TermOn|      |      |  Del |
+ * |      | Reset| Debug|ClyOn |ClyOff|      |      |TermOf|TermOn|      |      |  Del |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |      |Aud cy|Aud on|AudOff|AGnorm|AGswap|Qwerty|Colemk|Dvorak|      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -160,7 +160,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] = LAYOUT_preonic_grid(
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,        KC_F9,        KC_F10,       KC_F11,  KC_F12,
-  _______, RESET,   DEBUG,   _______, _______, _______, _______, TERM_ON,      TERM_OFF,     _______,      _______, KC_DEL,
+  _______, RESET,   DEBUG,   CK_ON,   CK_OFF,  _______, _______, TERM_ON,      TERM_OFF,     _______,      _______, KC_DEL,
   _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, DF(_QWERTY),  DF(_COLEMAK), DF(_DVORAK),  _______, _______,
   _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______,      _______,      _______,      _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______,      _______,      _______,      _______, _______
