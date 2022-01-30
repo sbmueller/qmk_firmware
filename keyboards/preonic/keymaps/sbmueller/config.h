649f5da59b04f901c35e7ed8ffa6c884fdfa87fe
Sebastian Müller <gsenpo@gmail.com>
2022-01-30 11:38:30 +0100

    Update
    - Clang format
    - Change taunttext button
    - Update colemak layout

--- keyboards/preonic/keymaps/sbmueller/config.h
+++ keyboards/preonic/keymaps/sbmueller/config.h
@@ -1,14 +1,12 @@
 #pragma once
 #define UNICODE_SELECTED_MODES UC_LNX, UC_MAC
 #ifdef AUDIO_ENABLE
-    #define AUDIO_INIT_DELAY
-    #define STARTUP_SONG SONG(PREONIC_SOUND)
-    // #define STARTUP_SONG SONG(NO_SOUND)
+#    define AUDIO_INIT_DELAY
+#    define STARTUP_SONG SONG(PREONIC_SOUND)
+// #define STARTUP_SONG SONG(NO_SOUND)
 
-    #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
-                                  SONG(COLEMAK_SOUND), \
-                                  SONG(DVORAK_SOUND) \
-                                }
+#    define DEFAULT_LAYER_SONGS \
+        { SONG(QWERTY_SOUND), SONG(COLEMAK_SOUND), SONG(DVORAK_SOUND) }
 #endif
 
 #define MUSIC_MASK (keycode != KC_NO)
