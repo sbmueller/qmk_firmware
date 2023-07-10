f3381826a7a3b7b78046e5afde280a4b7332edb2
Sebastian Müller <gsenpo@gmail.com>
2023-07-10 14:37:36 +0200

    Kyria: Remove curly braces from shift keys

--- keyboards/splitkb/kyria/keymaps/sbmueller/keymap.c
+++ keyboards/splitkb/kyria/keymaps/sbmueller/keymap.c
@@ -140,7 +140,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
  * |    ~   |  1   |  2   |  3   |  4   |  5   |                              |   6  |  7   |  8   |  9   |  0   |   ?    |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |    {   |   \  |  :   |  ;   |  =   |  +   |  {   |  [   |  |   ]  |   }  |   -  |  _   |  ,   |  .   |  |   |   }    |
+ * |        |   \  |  :   |  ;   |  =   |  +   |  {   |  [   |  |   ]  |   }  |   -  |  _   |  ,   |  .   |  |   |        |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
@@ -149,7 +149,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_SYM] = LAYOUT(
      KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
      KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_QUES ,
-     KC_LCBR, KC_BSLS, KC_COLN, KC_SCLN, KC_EQL,  KC_PLUS, KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, KC_MINS, KC_UNDS, KC_COMM, KC_DOT,  KC_PIPE, KC_RCBR,
+     _______, KC_BSLS, KC_COLN, KC_SCLN, KC_EQL,  KC_PLUS, KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, KC_MINS, KC_UNDS, KC_COMM, KC_DOT,  KC_PIPE, _______,
                                 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
